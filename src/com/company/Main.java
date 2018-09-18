b3efdaaf82985881c71bdf7f74e6fac7376edb31
l18732905600 <l18732905600@163.com>
2018-09-18 22:11:33 +0800

    cascsaccccCsasCsccccs

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("jdcsmcs");
+        System.out.println("jdcsdsccscscmcs");
 	// write your code here
     }
 }
