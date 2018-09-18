bbd8c50d7190103381fc505dec4513b25e660262
l18732905600 <l18732905600@163.com>
2018-09-18 22:39:06 +0800

    又一次提交 22:38  使用 分支 开始提交

--- src/com/company/Demo1.java
+++ src/com/company/Demo1.java
@@ -3,6 +3,7 @@ package com.company;
 public class Demo1 {
     public static void main(String[] args) {
         System.out.println(" you are  a good boy!");
+        say("cs");
     }
     public static void say(String name){
 
