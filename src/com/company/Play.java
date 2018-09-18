15c82e6413b1bd8a959a0e7a805990ae36f0cda2
l18732905600 <l18732905600@163.com>
2018-09-18 23:36:54 +0800

    又一次提交23:36 使用  开始提交

---
+++ src/com/company/Play.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Play {
+    private String ball;
+    private int num;
+
+    public String getBall() {
+        return ball;
+    }
+
+    public void setBall(String ball) {
+        this.ball = ball;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+}
