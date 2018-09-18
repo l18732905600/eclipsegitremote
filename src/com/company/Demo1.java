b43a4c87c2ee4790d633d75d66fdfd4c69652951
l18732905600 <l18732905600@163.com>
2018-09-18 22:29:17 +0800

    又一次提交 22:29

---
+++ src/com/company/Demo1.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println(" you are  a good boy!");
+    }
+    public static void say(String name){
+
+        System.out.println("yes!");
+
+    }
+}
